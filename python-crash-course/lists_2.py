8e20c70743cb4b8cabf2cc90da773ef79dcf5589
tmsunnyduck <tmsunducker@gmail.com>
2023-02-26 15:28:17 +0530

    snapshot commit

--- python-crash-course/lists_2.py
+++ python-crash-course/lists_2.py
@@ -12,3 +12,29 @@ for magician in magicians :
 
 for magician in magicians :
     print(f"{magician.title()}, that was a great trick!")
+    print(f"Thanks for performing {magician.title()}!")
+
+print("loop end")
+
+# Numerical lists
+
+# range(start,end,step) fucntion generates a series of
+# integers from start to (end - 1) adding the step value
+for value in range(1,5) : 
+    print(value)
+
+for even in range(2,11,2) : #adds 2 each iteration
+    print(even)
+# range() to make a list of numbers
+# use the list() to convert the result of range()
+# to a list
+
+numbers = list(range(1,6))
+print(numbers)
+
+cubes = []
+for number in range(1,11) :
+    cube = number ** 3
+    cubes.append(cube)
+
+print(cubes)
